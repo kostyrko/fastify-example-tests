684f0c2266cd04733937ed3e7493a92a6d66ff34
Mikołaj Kostyrko <mkostyrko@gmail.com>
2022-05-12 18:03:10 +0200

    spec01 test solution

--- cypress/integration/spec01.js
+++ cypress/integration/spec01.js
@@ -2,24 +2,12 @@
 /// <reference types="Cypress" />
 
 it('shows some fruit', () => {
-  // define baseUrl in cypress.json
-  // (if not already set)
-  //
-  // set the convenient viewport in cypress.json
-  // like viewportWidth: 400 and viewportHeight: 300
-  // (if not already set)
-  //
-  // visit the page
-  // https://on.cypress.io/visit
-  //
-  // get the fruit element by ID
-  // https://on.cypress.io/get
-  // and confirm it does not have the loading text
-  // and confirm it has the fruit name which is a word
-  // that starts with a capital letter
-  // followed by all lowercase letters
-  // https://on.cypress.io/should
-  //
-  // alternative: use cy.contains with a regular expression
-  // see https://on.cypress.io/contains
+  cy.visit('/');
+  cy.get('#fruit')
+  .should('not.contain', 'loading...')
+  .invoke('text')
+  .then(text => {
+    // expect(text).to.be.an('string')
+    expect(text).to.match(/^[A-Z][a-z]+$/)
+  })
 })
