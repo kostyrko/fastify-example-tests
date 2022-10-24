1385a4babacd08dec1f80284fbc3fb8a5c666079
Mikołaj <48617990+kostyrko@users.noreply.github.com>
2022-10-24 08:17:35 +0200

    add spec02.js solution

--- cypress/integration/spec02.js
+++ cypress/integration/spec02.js
@@ -6,11 +6,20 @@ it('shows the fruit returned by the server', () => {
   //
   // visit the page
   // https://on.cypress.io/visit
-  //
+  cy.intercept('/fruit').as('fruitGet')
+  cy.visit('/')
   // wait for the app to make the network call
   // (there might be a delay)
   // https://on.cypress.io/wait
-  //
+  cy.wait('@fruitGet').then((fruit) => {
+    // expect(fruit.response.statusCode).to.equal(200)
+    // cy.log(fruit.response.body.fruit)
+
+    // works as well but doesn't use assertion
+    cy.contains('#fruit', fruit.response.body.fruit)
+    
+    cy.get('#fruit').should('have.text', fruit.response.body.fruit)
+  })
   // from the network call, get the response body
   // and the name of the fruit and confirm
   // the fruit is shown on the page
