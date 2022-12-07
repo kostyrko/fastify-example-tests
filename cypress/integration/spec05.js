8a2cbe3832d4cc55dbe4dc8ce41bdb9da0166eee
Mikołaj <48617990+kostyrko@users.noreply.github.com>
2022-12-07 13:36:17 +0100

    add spec05 solution

--- cypress/integration/spec05.js
+++ cypress/integration/spec05.js
@@ -2,12 +2,20 @@
 
 it('shows a different fruit after reloading the page', () => {
   // visit the site using https://on.cypress.io/visit
+  cy.visit('/')
   // grab the fruit name from the page
   // (make sure it is not "loading...")
-  //
-  // reload the page using https://on.cypress.io/reload
-  // grab the fruit name from the page again
-  // confirm the fruit name is different
-  //
-  // tip: use nested https://on.cypress.io/then callbacks
+  cy.get('#fruit')
+    .should('not.include.text', 'loading')
+    .invoke('text')
+    .then(cy.log)
+    .then((fruit) => {
+      // reload the page using https://on.cypress.io/reload
+      cy.reload()
+      // grab the fruit name from the page again
+      // confirm the fruit name is different
+      cy.get('#fruit')
+        .should('not.include.text', 'loading')
+        .and('not.have.text', fruit)
+    })
 })
