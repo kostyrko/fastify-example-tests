41a33b9ff834f004ef99685e2248974ea48b717c
Mikołaj <48617990+kostyrko@users.noreply.github.com>
2022-12-13 14:10:25 +0100

    add spec 11 and 12 solutions

--- cypress/integration/spec11.js
+++ cypress/integration/spec11.js
@@ -11,4 +11,20 @@ it('makes GET /fruit requests every minute', () => {
   // wait for the second request to finish and confirm the fruit
   // advance the clock by one minute one more time
   // wait for the network call and confirm the fruit
+  cy.intercept('GET', '/fruit').as('fruit')
+  cy.clock()
+  cy.visit('/')
+  cy.wait('@fruit').its('response.body.fruit').then((fruit) => {
+    cy.contains('#fruit', fruit).should('be.visible')
+  })
+  cy.tick(60_000)
+  cy.wait('@fruit').its('response.body.fruit').then((fruit) => {
+    cy.contains('#fruit', fruit).should('be.visible')
+  })
+  cy.tick(60_000)
+  cy.wait('@fruit').its('response.body.fruit').then((fruit) => {
+    cy.contains('#fruit', fruit).should('be.visible')
+  })
+
+
 })
--- cypress/integration/spec12.js
+++ cypress/integration/spec12.js
@@ -16,4 +16,15 @@ it('does not call API until 60 seconds passed', () => {
   // confirm the spy was not executed = there were no network calls
   // tick 1 second
   // confirm the spy was executed = there was a network call
+  cy.intercept('GET', '/fruit', cy.spy().as('fruit'))
+  cy.clock()
+  cy.visit('/')
+
+  cy.get('@fruit')
+    .should('have.been.calledOnce')
+    .invoke('resetHistory')
+  cy.tick(59_000)
+  cy.get('@fruit').should('not.have.been.called')
+  cy.tick(1_000)
+  cy.get('@fruit').should('have.been.calledOnce')
 })
