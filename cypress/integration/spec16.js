08b72334c2b69dca9a9d9066e48dd72190480569
Mikołaj <48617990+kostyrko@users.noreply.github.com>
2022-12-16 09:05:40 +0100

    add 16 and 17 solution

--- cypress/integration/spec16.js
+++ cypress/integration/spec16.js
@@ -8,4 +8,19 @@ it('requests all fruits', () => {
   // store each fruit in a Set object
   // and keep requesting until we see a fruit already in the set
   // print the collected list of fruits
+  const fruits = new Set()
+  function getTheFruit() {
+    cy.request('/fruits').then(response => {
+      its('body.fruit')
+      .then(fruits => {
+        if (fruit.has(fruit)) {
+          ([...fruits].sort().join(', '))
+        } else {
+          fruits.add(fruit)
+          cy.wait(1000).then(getTheFruit)
+        }
+      })
+    })
+  }
+  getTheFruit()
 })
--- cypress/integration/spec17.js
+++ cypress/integration/spec17.js
@@ -9,4 +9,25 @@ it('requests the fruits N times', () => {
   // and confirm the list of fruits is the same as expected
   // ['Apples', 'Bananas', 'Grapes', 'Oranges', 'Pears']
   // hint: use Cypress._.times
+  const fruits = []
+  Cypress._.times(5, () => {
+    cy.request('GET', '/fruit')
+      .its('body.fruit')
+      .then((fruit) => {
+        fruits.push(fruit)
+      })
+  })
+
+  cy.wrap(fruits).then(JSON.stringify).then(cy.log)
+  cy.wrap(fruits)
+    .should('have.length', 5)
+    // sort the array before comparing
+    .invoke('sort')
+    .should('deep.equal', [
+      'Apples',
+      'Bananas',
+      'Grapes',
+      'Oranges',
+      'Pears',
+    ])
 })
